800919932c83b9a338f2717c1172378181bca173
Jivvie <gjvon@mitnov.com>
2017-01-03 07:43:26 -0600

    Added new functions

--- scripts/ProductObject.js
+++ scripts/ProductObject.js
@@ -6,6 +6,9 @@ var ProductObject = (function () {
     /* Constructor to initialize variables we need for our Product */
     function ProductObject(productType, productMaterial, doubleSided, width, length) {
         this.baseCost = 0;
+        this.materialConstant = 1;
+        this.length = 0;
+        this.width = 0;
         this.width = width;
         this.length = length;
         this.productMaterial = productMaterial;
@@ -20,10 +23,20 @@ var ProductObject = (function () {
         not be the same value. TODO: Look into this soon 12/2/2017 */
         this.productMaterial = this.productMaterial.toLowerCase();
         if (this.productMaterial == "metallic") {
+            this.materialConstant = 2;
         }
     };
     ProductObject.prototype.calculateArea = function () {
         return (this.length * this.width);
+    };
+    ProductObject.prototype.computeTotalCost = function () {
+        var areaCost = (this.materialConstant * this.calculateArea());
+        /** TODO: Double sided is left abstract right now. If the customer wants
+        a double-sided product we should give some type of discount. I will apply a
+        25% discount for now.*/
+        var discount = .25;
+        areaCost = areaCost + (areaCost * discount);
+        return areaCost;
     };
     /** Check for numbers and white spaces and invalid characters. */
     //TODO:  Make sure this is defined upon production.
--- scripts/ProductObject.ts
+++ scripts/ProductObject.ts
@@ -8,8 +8,9 @@ class ProductObject {
 	doubleSided:boolean;
 	productMaterial:string;
 	baseCost: number = 0;
-	length: number;
-	width:number;
+	materialConstant: number = 1;
+	length: number = 0;
+	width:number = 0;
 
 	/* Constructor to initialize variables we need for our Product */
 	constructor(productType:string, productMaterial:string, doubleSided: number,
@@ -31,13 +32,25 @@ class ProductObject {
 		not be the same value. TODO: Look into this soon 12/2/2017 */
 		this.productMaterial = this.productMaterial.toLowerCase();
 		if(this.productMaterial == "metallic") {
-			//
+			this.materialConstant = 2;
 		}
 	}
 
 	calculateArea()
 	{
 		return (this.length * this.width);
+	}
+
+	computeTotalCost()
+	{
+		var areaCost:any = (this.materialConstant * this.calculateArea());
+		/** TODO: Double sided is left abstract right now. If the customer wants 
+		a double-sided product we should give some type of discount. I will apply a 
+		25% discount for now.*/
+		var discount:number = .25;
+
+		areaCost = areaCost + (areaCost * discount);
+		return areaCost;
 	}
 
 	/** Check for numbers and white spaces and invalid characters. */
